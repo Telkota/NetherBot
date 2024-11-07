d8e86899009b1f23f0f51c8e61688c5c3d9c7328
Ole <26010011+Telkota@users.noreply.github.com>
2024-11-07 20:29:07 +0100

    FEAT: Conversion done
    Successfully converted the bot to use @bot instead of @client.
    Re-organized code a little bit

--- bot.py
+++ bot.py
@@ -1,30 +1,31 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
-
-# bot needs message and members intents
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
 
 #grab token from .env file
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-client = discord.Client(intents=intents)
+# bot needs message and members intents in the developer portal
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 #List of channel names to check for welcome message - Switch out or add names as needed
 welcome_channel_names = ["social", "general"]
 
 #on startup message in console
-@client.event
+@bot.event
 async def on_ready():
-    print(f"\n{client.user} is connected to the following guild:")
-    for guild in client.guilds:
+    print(f"\n{bot.user} is connected to the following guild:")
+    for guild in bot.guilds:
         print(f"server name: {guild.name} (id: {guild.id})\n")
 
 #welcome function
-@client.event
+@bot.event
 async def on_member_join(member):
     for channel in member.guild.channels:
         if channel.name in welcome_channel_names:
@@ -35,4 +36,13 @@ async def on_member_join(member):
                                "If you have any questions feel free to DM any officer/admin!")
 
 
-client.run(TOKEN)
+#error handling
+@bot.event
+async def on_error(event, *args, **kwargs):
+    with open("err.log", "a") as f:
+        if event == "on_message":
+            f.write(f"unhandled message: {args[0]}\n")
+        else:
+            raise
+
+bot.run(TOKEN)
