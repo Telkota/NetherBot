0ea0d3207c8f008910b7223ff336b87d534ca775
Ole <26010011+Telkota@users.noreply.github.com>
2024-11-08 19:31:18 +0100

    Feat: Moderation.py changes
    Fixed some bugs regarding permissions.
    (has_permissions -> has_guild_permissions)
    added mute, deafen, unmute and undeafen.

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 import os
 import discord
+import logging
+import traceback
 from discord.ext import commands
 from dotenv import load_dotenv
+
+#setup logging
+logging.basicConfig(level=logging.ERROR)
 
 #grab token from .env file
 load_dotenv()
@@ -11,6 +16,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.voice_states = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -21,6 +27,12 @@ async def on_ready():
     print(f"\n{bot.user} is connected to the following guild:")
     for guild in bot.guilds:
         print(f"server name: {guild.name} (id: {guild.id})\n")
+
+        #for testing purposes: Bot Permissions. Remove later
+        bot_member = guild.get_member(bot.user.id)
+        for channel in guild.channels:
+            permissions = channel.permissions_for(bot_member)
+            print(f"Channel: {channel.name}, Permissions: {permissions}")
 
 #Import commands from other modules
 from commands import welcome, moderation
@@ -34,9 +46,17 @@ moderation.setup(bot)
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send("Invalid command used. Please check the command and try again.")
+        await ctx.send(f"Invalid command. Please check the command and try again.\n"
+                       "If you don't know what commands I have you can check out `!help` to get a list of commands")
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send("You don't have the required permissions to run this command. ")
     else:
-        #handle other errors that may occur
-        await ctx.send("An error occurred while processing the command.")
+        #Log the error details in the console
+        logging.error(f"An error occured: {error}")
+        traceback.print_exception(type(error), error, error.__traceback__)
+        
+        #Send an error message in Discord
+        await ctx.send(f"An error occurred while processing the command:\n"
+                       f"{error}")
 
 bot.run(TOKEN)
--- commands/moderation.py
+++ commands/moderation.py
@@ -1,121 +1,210 @@
 from discord.ext import commands
 import discord
 
+
 def setup(bot):
     @bot.command(name="moveall", help="Move all members current connected to a voice channel into your specified channel")
-    @commands.has_permissions(move_members=True)
-    async def move_all(ctx, target_channel_name: str):
-        target_channel = None
-        target_name_lower = target_channel_name.lower()
-
-        #Find the target channel by partial name match. Case insensitive
-        for vc in ctx.guild.voice_channels:
-            if target_name_lower in vc.name.lower():
-                target_channel = vc
-                break
+    @commands.has_guild_permissions(move_members=True)
+    async def move_all(ctx, target_channel_name: str = None):
+        #if no target parameter is entered, return an error message and quit.
+        if target_channel_name is None:
+            await ctx.send("Error: No target channel name provided. Please specify a target channel.")
+            return
         
-        #check to see if the target channel exists
-        if target_channel:
-            for voice_channel in ctx.guild.voice_channels:          #loop over each voice channel
-                if voice_channel != target_channel:                 #as long as the channel is not the target
-                    for member in voice_channel.members:            #move each member in that channel into the target
-                        await member.move_to(target_channel)
-            #confirmation message that it was successful
-            await ctx.send(f"All members moved to {target_channel.name} from other voice channels")
-        #if the target channel is invalid
-        else:
-            await ctx.send("Invalid target channel name provided.")
+        try:
+            target_channel = None
+            target_name_lower = target_channel_name.lower()
+
+            #Find the target channel by partial name match. Case insensitive
+            for vc in ctx.guild.voice_channels:
+                if target_name_lower in vc.name.lower():
+                    target_channel = vc
+                    break
+            
+            #check to see if the target channel exists
+            if target_channel:                
+                for voice_channel in ctx.guild.voice_channels:          #loop over each voice channel
+                    if voice_channel != target_channel:                 #as long as the channel is not the target
+                        for member in voice_channel.members:            #move each member in that channel into the target
+                            await member.move_to(target_channel)
+
+                #confirmation message that it was successful
+                await ctx.send(f"All members moved to {target_channel.name} from other voice channels")
+
+            #if the target channel is invalid
+            else:
+                await ctx.send("Invalid target channel name provided.")
+
+        except Exception as e:
+            await ctx.send("An error occurred while trying to move users.")
+            print(f"Error in move_all: {e}")
 
     @bot.command(name="move", help="Move a member to a different voice channel")
     @commands.has_guild_permissions(move_members=True)
-    async def move(ctx, member: discord.Member, target_channel_name: str):
-        target_channel = None
-        target_name_lower = target_channel_name.lower()
-
-        #Find the target channel by partial name match. Case insensitive
-        for vc in ctx.guild.voice_channels:
-            if target_name_lower in vc.name.lower():
-                target_channel = vc
-                break
+    async def move(ctx, member: discord.Member = None, target_channel_name: str = None):
+        #if no target or member parameter is entered, return an error message and quit.
+        if member is None and target_channel_name is None:
+            await ctx.send(f"Error: You didn't specify a user nor target channel.\n"
+                           "To use this command, provide a user and target channel.\n"
+                           "`!move <user> <target-channel>`")
+            return
+        elif target_channel_name is None:
+            await ctx.send("Error: You didn't specify a target channel.")
+            return
         
-        #check to see if the target channel exists
-        if target_channel:
-            await member.move_to(target_channel)
-            await ctx.send(f"Moved {member} to {target_channel.name}")
-        #if the target channel is invalid
-        else:
-            await ctx.send("Invalid target channel name provided.")
+        try:
+            target_channel = None
+            target_name_lower = target_channel_name.lower()
+
+            #Find the target channel by partial name match. Case insensitive
+            for vc in ctx.guild.voice_channels:
+                if target_name_lower in vc.name.lower():
+                    target_channel = vc
+                    break
+            
+            #check to see if the target channel exists
+            if target_channel:
+                await member.move_to(target_channel)
+                await ctx.send(f"Moved {member} to {target_channel.name}")
+
+            #if the target channel is invalid
+            else:
+                await ctx.send("Invalid target channel name provided.")
+
+        except Exception as e:
+            await ctx.send("An error occured while trying to move the user")
+            print(f"Error in move: {e}")
     
     @bot.command(name="kick", help="Kick a member")
-    @commands.has_permissions(kick_members=True)
-    async def kick(ctx, member: discord.Member, *, reason=None):
-        await member.kick(reason=reason)
-        #if reason is given - Display reason
-        if reason is not None:
-            await ctx.send(f"Kicked {member}. Reason: {reason}")
-        #Otherwise - Display just kick message
-        else:
-            await ctx.send(f"Kicked {member}")
+    @commands.has_guild_permissions(kick_members=True)
+    async def kick(ctx, member: discord.Member = None, *, reason=None):
+        #if no member paramter is entered, return an error message and quit.
+        if member is None:
+            await ctx.send("Error: You didn't input a user to kick.")
+            return
+        
+        try:
+            await member.kick(reason=reason)
+            #if reason is given - Display reason
+            if reason is not None:
+                await ctx.send(f"Kicked {member}. Reason: {reason}")
+            #Otherwise - Display just kick message
+            else:
+                await ctx.send(f"Kicked {member}")
+
+        except Exception as e:
+            await ctx.send("An error occured while trying to kick the user.")
+            print(f"Error in kick: {e}")
 
     @bot.command(name="ban", help="Ban a member")
-    @commands.has_permissions(ban_members=True)
-    async def ban(ctx, member: discord.Member, *, reason=None):
-        await member.ban(reason=reason)
-        #if reason is given - Display reason
-        if reason is not None:
-            await ctx.send(f"Banned {member}. Reason: {reason}")
-        #Otherwise - Display just ban message
-        else:
-            await ctx.send(f"Banned {member}")
+    @commands.has_guild_permissions(ban_members=True)
+    async def ban(ctx, member: discord.Member = None, *, reason=None):
+        #if no member paramter is entered, return an error message and quit.
+        if member is None:
+            await ctx.send("Error: You didn't input a user to ban.")
+            return
+        
+        try:
+            await member.ban(reason=reason)
+            #if reason is given - Display reason
+            if reason is not None:
+                await ctx.send(f"Banned {member}. Reason: {reason}")
+            #Otherwise - Display just ban message
+            else:
+                await ctx.send(f"Banned {member}")
+
+        except Exception as e:
+            await ctx.send("An error occured while trying to ban the user.")
+            print(f"Error in ban: {e}")
 
     @bot.command(name="mute", help="Mute a member in voice")
-    @commands.has_permissions(mute_members=True)
-    async def mute(ctx, member: discord.Member):
-        #check if the user is in a voice channel
-        if member.voice is None:
-            await ctx.send(f"{member} is not in a voice channel. Muting is unnecessary.")
-            return
-        #check if the user is already muted
-        if member.voice.mute:
-            await ctx.send(f"{member} is already muted.")
+    @commands.has_guild_permissions(mute_members=True)
+    async def mute(ctx, member: discord.Member = None):
+        #if no member paramter is entered, return an error message and quit.
+        if member is None:
+            await ctx.send("Error: You didn't input a user to mute.")
             return
         
-        await member.edit(mute=True)
-        await ctx.send(f"Muted {member}")
+        try:
+            #check if the user is in a voice channel
+            if member.voice is None:
+                await ctx.send(f"{member} is not in a voice channel. Muting is unnecessary.")
+                return
+            #check if the user is already muted
+            if member.voice.mute:
+                await ctx.send(f"{member} is already muted.")
+                return
+            
+            await member.edit(mute=True)
+            await ctx.send(f"Muted {member}")
+
+        except Exception as e:
+            await ctx.send("An error occured while trying to mute the user.")
+            print(f"Error in mute: {e}")
     
     @bot.command(name="deafen", help="Deafen a member in voice")
-    @commands.has_permissions(deafen_members=True)
-    async def deafen(ctx, member: discord.Member):
-        #check if the user is in a voice channel
-        if member.voice is None:
-            await ctx.send(f"{member} is not in a voice channel. Muting is unnecessary.")
-            return
-        #check if the user is already deafened
-        if member.voice.deaf:
-            await ctx.send(f"{member} is already deafened.")
+    @commands.has_guild_permissions(deafen_members=True)
+    async def deafen(ctx, member: discord.Member = None):
+        #if no member paramter is entered, return an error message and quit.
+        if member is None:
+            await ctx.send("Error: You didn't input a user to deafen.")
             return
         
-        await member.edit(deafen=True)
-        await ctx.send(f"Deafened {member}")
+        try:
+            #check if the user is in a voice channel
+            if member.voice is None:
+                await ctx.send(f"{member} is not in a voice channel. Muting is unnecessary.")
+                return
+            #check if the user is already deafened
+            if member.voice.deaf:
+                await ctx.send(f"{member} is already deafened.")
+                return
+            
+            await member.edit(deafen=True)
+            await ctx.send(f"Deafened {member}")
+
+        except Exception as e:
+            await ctx.send("An error occured while trying to deafen the user.")
+            print(f"Error in deafen: {e}")
     
     @bot.command(name="unmute", help="Unmute a member in voice")
-    @commands.has_permissions(mute_members=True)
-    async def unmute(ctx, member: discord.Member):
-        #check if the user is already unmuted
-        if not member.voice.mute:
-            await ctx.send(f"{member} is already unmuted.")
-            return
-
-        await member.edit(mute=False)
-        await ctx.send(f"Unmuted {member}")
-    
-    @bot.command(name="undeafen", help="Undeafen a member in voice")
-    @commands.has_permissions(deafen_members=True)
-    async def undeafen(ctx, member: discord.Member):
-        #check if the user is already undeafened
-        if not member.voice.deaf:
-            await ctx.send(f"{member} is already undeafened.")
+    @commands.has_guild_permissions(mute_members=True)
+    async def unmute(ctx, member: discord.Member = None):
+        #if no member paramter is entered, return an error message and quit.
+        if member is None:
+            await ctx.send("Error: You didn't input a user to unmute.")
             return
         
-        await member.edit(deafen=False)
-        await ctx.send(f"Undeafened {member}")
+        try:
+            #check if the user is already unmuted
+            if not member.voice.mute:
+                await ctx.send(f"{member} is already unmuted.")
+                return
+
+            await member.edit(mute=False)
+            await ctx.send(f"Unmuted {member}")
+
+        except Exception as e:
+            await ctx.send("An error occured while trying to unmute the user.")
+            print(f"Error in unmute: {e}")
+    
+    @bot.command(name="undeafen", help="Undeafen a member in voice")
+    @commands.has_guild_permissions(deafen_members=True)
+    async def undeafen(ctx, member: discord.Member = None):
+        #if no member paramter is entered, return an error message and quit.
+        if member is None:
+            await ctx.send("Error: You didn't input a user to undeafen.")
+            return
+        
+        try:
+            #check if the user is already undeafened
+            if not member.voice.deaf:
+                await ctx.send(f"{member} is already undeafened.")
+                return
+            
+            await member.edit(deafen=False)
+            await ctx.send(f"Undeafened {member}")
+
+        except Exception as e:
+            await ctx.send("An error occured while trying to undeafen the user.")
+            print(f"Error in undeafen: {e}")
