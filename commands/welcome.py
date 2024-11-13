ad3b8c6097ae95aed9578286c799ba8e50cb1a83
Ole <26010011+Telkota@users.noreply.github.com>
2024-11-13 15:00:18 +0100

    Feat: Check config.json for welcome message
    Welcome.py will try to open the file config.json to look for the "welcome_message" variable and its' value.
    if it's empty or doesn't exist then it'll use a generic message instead.

--- commands/welcome.py
+++ commands/welcome.py
@@ -1,4 +1,6 @@
 import discord
+import json
+import logging
 from discord.ext import commands
 
 class Welcome(commands.Cog):
@@ -10,9 +12,30 @@ class Welcome(commands.Cog):
     async def on_member_join(self, member):
         channel = member.guild.system_channel
         if channel is not None:
-            await channel.send(f"Welcome to {member.guild.name}, {member.mention}!\n"
-                               "No specific rules to mention.\n"
-                               "If you have any questions, feel free to DM any officer/admin!")
+            #Variable to store the base greeting
+            welcome_user = f"Welcome to {member.guild.name}, {member.mention}!\n"
+            generic_message = "No rules to mention - If you have any questions, feel free to ask any officer/admin!"
+            try:
+                #Check for existing welcome message within config.json
+                with open("config.json", "r") as file:
+                    config = json.load(file)
+                
+                #Add the welcome message into a variable if it exists, if not add a generic message
+                welcome_message = config.get("welcome_message", generic_message)
+
+                #If the welcome_message variable is empty, then add in generic_message
+                if not welcome_message:
+                    welcome_message = generic_message
+
+            except (FileNotFoundError, json.JSONDecodeError):
+                welcome_message = generic_message
+            
+            full_message = welcome_user + welcome_message
+            await channel.send(full_message)
+            
+        else:
+            #Log a message if system channel is not found, or the bot doesn't have access.
+            print(f"System channel not found or are inaccessible. (Server: {member.guild.name})")
 
 async def setup(bot):
     await bot.add_cog(Welcome(bot))
